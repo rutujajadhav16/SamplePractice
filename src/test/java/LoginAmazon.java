55448a12cf190d9c3b1cf49ae0e4c8de6b4d0b89
Admin <Admin@DESKTOP-SH023E5>
2023-07-20 17:54:55 +0530

    Amazon Commit

---
+++ src/test/java/LoginAmazon.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginAmazon {
+  @Test
+  public void f() {
+	  
+	  WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+		driver.manage().window().maximize();
+  }
+}
